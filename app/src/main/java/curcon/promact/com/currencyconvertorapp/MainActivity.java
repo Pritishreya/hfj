d1245a1093eab507d45a62328713f770aeae72c0
Priti Shreya <priti@promact.com>
2018-02-14 18:52:23 +0530

    c

---
+++ app/src/main/java/curcon/promact/com/currencyconvertorapp/MainActivity.java
@@ -0,0 +1,15 @@
+package curcon.promact.com.currencyconvertorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView Japanese_Yen,Indian_Rupee, ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
